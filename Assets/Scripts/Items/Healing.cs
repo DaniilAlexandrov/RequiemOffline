413e3655a0e8f69ea821e0fd2edd3b90bded6a07
WorryWarrior <worrywarrior1@gmail.com>
2020-06-07 14:18:01 +0300

    There and back again.

---
+++ Assets/Scripts/Items/Healing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healing : MonoBehaviour
+{
+    [SerializeField] private int healingAmount = 0;
+    [SerializeField] private int numberOfUsages = 0;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth targetHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            if (targetHealth != null && targetHealth.Health != targetHealth.MaxHealth)
+            {
+                targetHealth.GainHealth(healingAmount);
+                numberOfUsages--;
+                if (numberOfUsages == 0)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+        }
+
+    }
+}
